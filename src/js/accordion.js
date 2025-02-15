1b3115d8ebc5d072c9d48d8d5a2cbd8c841709e9
Pedro Henrique Nunes <160438856+PedroHenri10@users.noreply.github.com>
2025-02-14 22:20:35 -0300

    arrumando accordion js

--- src/js/accordion.js
+++ src/js/accordion.js
@@ -3,6 +3,13 @@ const accordions = document.querySelectorAll('.accordion');
 accordions.forEach(accordion => {
     accordion.addEventListener("click", () => {
         const body = accordion.querySelector(".accordion-body");
+
+        accordions.forEach(otherAccordion => {
+            if (otherAccordion !== accordion) {
+                otherAccordion.querySelector(".accordion-body").classList.remove('active');
+            }
+        });
+
         body.classList.toggle('active');
     });
 });
